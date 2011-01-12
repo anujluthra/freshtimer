146027badc399e426fbdab62c641e4f0f90546f6
anuj <anuj@36zeroes.com.au>
2011-01-13 00:16:15 +1030

    datamapper resource for time entries

---
+++ lib/slim_timer_entry.rb
@@ -0,0 +1,34 @@
+class SlimTimerEntry
+  include DataMapper::Resource
+
+  property :id,                   Serial
+  property :duration_in_seconds,	Integer
+  property :sync_id,              Integer, :required => true, :unique => true
+  property :comments,             String
+  property :start_time,           DateTime, :required => true
+  property :end_time,             DateTime, :required => true
+  property :task_name,            String,   :required => true
+
+  
+
+  def self.sync_with_remote
+    synced = 0
+    SLIM_TIMER.list_timeentries.each{|record|
+      begin
+        create(
+          :sync_id =>             record['id'],
+          :duration_in_seconds => record['duration_in_seconds'].to_i,
+          :comments =>            record['comments'],
+          :start_time =>          record['start_time'],
+          :end_time =>            record['end_time'],
+          :task_name =>           record['task']['name']
+        )
+        synced += 1
+      rescue DataObjects::IntegrityError => e
+        print "*"
+      end
+    }
+    puts "Successfully synced #{synced} new time entries"
+  end
+
+end

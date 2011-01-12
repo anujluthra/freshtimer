f9f6f9387844e36994fa7f6d31bab848808eaee5
anuj <anuj@36zeroes.com.au>
2011-01-13 00:16:58 +1030

    Can pull data from SlimTimer

---
+++ sync.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'dm-core'
+require 'dm-migrations'
+require 'yaml'
+require 'slimtimer4r'
+
+# If you want the logs displayed you have to do this before the call to setup
+DataMapper::Logger.new($stdout, :debug)
+
+# A Sqlite3 connection to a persistent database
+DataMapper.setup(:default, 'sqlite://' + Dir.pwd + '/' + 'freshtimer.db')
+
+load 'lib/slim_timer_entry.rb'
+
+# create the db schema with properties in models
+#DataMapper.auto_migrate!
+
+DataMapper.finalize
+
+CONFIG = YAML.load_file('config.yaml')
+SLIM_TIMER = SlimTimer.new(*CONFIG['slimtimer'].values)
+
+SlimTimerEntry.sync_with_remote
+
+
+
+
+

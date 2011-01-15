9e1c52fb7913794b4e42307dba5277c1426000bc
anuj <anuj@36zeroes.com.au>
2011-01-15 15:46:51 +1030

    sync task now uploads to freshbooks

--- sync.rb
+++ sync.rb
@@ -1,27 +1,8 @@
-require 'rubygems'
-require 'dm-core'
-require 'dm-migrations'
-require 'yaml'
-require 'slimtimer4r'
-
-# If you want the logs displayed you have to do this before the call to setup
-DataMapper::Logger.new($stdout, :debug)
-
-# A Sqlite3 connection to a persistent database
-DataMapper.setup(:default, 'sqlite://' + Dir.pwd + '/' + 'freshtimer.db')
-
-load 'lib/slim_timer_entry.rb'
-
-# create the db schema with properties in models
-DataMapper.auto_upgrade!
-
-DataMapper.finalize
-
-CONFIG = YAML.load_file('config.yaml')
-SLIM_TIMER = SlimTimer.new(*CONFIG['slimtimer'].values)
+load 'init.rb'
 
 SlimTimerEntry.sync_with_remote
-
+FreshBooksEntry.sync_to_remote(SlimTimerEntry.unsynced)
+puts "All done ... bye"
 
 
 
